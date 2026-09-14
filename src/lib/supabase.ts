6fd4c8173fc031f6e6f3bf72d296801b0613ff1a
Mugunthan107 <mugunthnirmal@gmail.com>
2026-09-14 19:52:05 +0530

    add supabase connection for vercel

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = (import.meta.env.VITE_SUPABASE_URL as string) || '';
-const supabaseKey = (import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY as string) || '';
+const supabaseUrl =
+  (import.meta.env.VITE_SUPABASE_URL as string) ||
+  'https://snhllplycrumbyqwqqpb.supabase.co';
+const supabaseKey =
+  (import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY as string) ||
+  'sb_publishable_qdCeKrPLdpAWX8a2sYhoxA_guDNEHEp';
 
 export const isSupabaseConfigured = Boolean(
   supabaseUrl &&
